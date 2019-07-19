0c8062b691a6708227f04bb8ac90fe3e5b029b41
saaucorp <shuhaila@springrainit.com>
2019-07-19 10:30:38 +0600

    build script updated

--- app_modules/index.js
+++ app_modules/index.js
@@ -1,5 +1,6 @@
 // Authentication for child entities
 module.exports = async ctx => {
+  console.log(ctx.request);
   // ctx contains request, response, dispatch, schema, method & much more...
   // check documentation
 };
